61e1c9e13526b0c34ea43755f366976e9a864330
Hudki <42683289+Hudki@users.noreply.github.com>
2019-08-07 12:48:27 -0400

    Syntax fix

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import pyaudio  # pip install pyaudio
 import numpy as np  # np varial name
 import scipy  # pip install scipy
-import matplotlib.pylot as plt  # pip install matplotlib
+import matplotlib.pyplot as plt  # pip install matplotlib
 
 CHANNELS = 1
 CHUNK = 4096  # number of data points to read at a time
@@ -16,14 +16,15 @@ stream=p.open(format=pyaudio.paInt16,
     frames_per_buffer=CHUNK)
 
 
-while True:
-    try:  # unparsed data to see if mic works
+try:
+    print('Press a key to close')
+    while True:  # unparsed data to see if mic works
         data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
         peak=np.average(np.abs(data))*2
         bars="▬"*int(50*peak/2**16)
         print("%04d %05d %s"%(peak,bars))   # prints audio level as bars
 
-    except KeyboardInterrupt:  # closes on keystroke detection
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+except KeyboardInterrupt:  # closes on keystroke detection
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
