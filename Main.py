685f318815dfc544b26eee85bac551a1dcfad98c
Hudki <42683289+Hudki@users.noreply.github.com>
2019-08-07 13:29:37 -0400

    Added FFT on data stream from audio as function

--- Main.py
+++ Main.py
@@ -4,27 +4,49 @@ import scipy  # pip install scipy
 import matplotlib.pyplot as plt  # pip install matplotlib
 
 CHANNELS = 1
-CHUNK = 4096  # number of data points to read at a time
 RATE = 44100  # Sample Rate
-
-p=pyaudio.PyAudio()  # start the PyAudio class & uses default input device
-stream=p.open(format=pyaudio.paInt16,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    output=False,
-    frames_per_buffer=CHUNK)
+t = 0.1  # seconds of sampling
+CHUNK = RATE*t   # number of data points to read at a time
 
 
-try:
-    print('Press a key to close')
-    while True:  # unparsed data to see if mic works
-        data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-        peak=np.average(np.abs(data))*2
-        bars="▬"*int(50*peak/2**16)
-        print("%04d %05d %s"%(peak,bars))   # prints audio level as bars
+def applyfft(stream): # FFT on data stream
+    # FFT
+    Y_k = np.fft.fft(stream)[0:int(CHUNK/2)]/CHUNK # FFT function from numpy
+    Y_k[1:] = 2*Y_k[1:] # need to take the single-sided spectrum only
+    Pxx = np.abs(Y_k) # be sure to get rid of imaginary part
+    freqvector = RATE*np.arange((CHUNK/2))/CHUNK;
 
-except KeyboardInterrupt:  # closes on keystroke detection
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+    # PLOT, may need to move to main instead of having in fft function
+    fig,ax = plt.subplots()
+    plt.plot(freqvector,Pxx,linewidth=5)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Frequency [Hz]')
+    plt.show()
+
+
+if __name__=="__main__":
+    p=pyaudio.PyAudio()  # start the PyAudio class & uses default input device
+    stream=p.open(format=pyaudio.paInt16,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        output=False,
+        frames_per_buffer=CHUNK)
+
+    try:
+        print('Press a key to close...')
+        while True:  # unparsed data to see if mic works
+            data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+            peak=np.average(np.abs(data))*2
+            bars="▬"*int(50*peak/2**16)
+            print("%04d %05d %s"%(peak,bars))   # prints audio level as bars
+
+
+
+
+    except KeyboardInterrupt:  # closes on keystroke detection
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
