42b8b6d87038ca8d235cb5fe2430de148802c299
Hudki <42683289+Hudki@users.noreply.github.com>
2019-08-07 10:58:17 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,17 @@
 import pyaudio  # pip install pyaudio
-import numpy as np  #np varial name
+import numpy as np  # np varial name
+import time
+import pylab
 
 CHUNK = 4096  # number of data points to read at a time
-RATE = 44100  # time resolution of the recording device (Hz)
+RATE = 44100  # Sample Rate
 
+p=pyaudio.PyAudio()  # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK)  #uses default input device
 
 # unparsed data to see if mic works
-for i in range(10):  #to it a few times just to see
+for i in range(10):  # Read multiple data
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
     print(data)
 
