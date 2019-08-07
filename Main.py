bf3466661bc1be7bce019975d3f51f7963698951
Hudki <42683289+Hudki@users.noreply.github.com>
2019-08-07 11:53:09 -0400

    Adding peak and bars

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import pyaudio  # pip install pyaudio
 import numpy as np  # np varial name
-import time
-import pylab
+import scipy  # pip install scipy
+import matplotlib  # pip install matplotlib
+
 
 CHUNK = 4096  # number of data points to read at a time
 RATE = 44100  # Sample Rate
@@ -11,9 +12,11 @@ stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK)  #uses default input device
 
 # unparsed data to see if mic works
-for i in range(10):  # Read multiple data
+for i in range(20*44100/1024):  # Read multiple data
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    print(data)
+    peak=np.average(np.abs(data))*2
+    bars="▬"*int(50*peak/2**16)
+    print("%04d %05d %s"%(i,peak,bars))
 
 # close the stream gracefully
 stream.stop_stream()
