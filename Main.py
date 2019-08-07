cb8c5840994bbd6dc41d7c6e3a9b3c35dba26d4e
Hudki <42683289+Hudki@users.noreply.github.com>
2019-08-07 12:43:17 -0400

    Close stream with keyboard detection

--- Main.py
+++ Main.py
@@ -1,24 +1,29 @@
 import pyaudio  # pip install pyaudio
 import numpy as np  # np varial name
 import scipy  # pip install scipy
-import matplotlib  # pip install matplotlib
+import matplotlib.pylot as plt  # pip install matplotlib
 
-
+CHANNELS = 1
 CHUNK = 4096  # number of data points to read at a time
 RATE = 44100  # Sample Rate
 
-p=pyaudio.PyAudio()  # start the PyAudio class
-stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
-              frames_per_buffer=CHUNK)  #uses default input device
+p=pyaudio.PyAudio()  # start the PyAudio class & uses default input device
+stream=p.open(format=pyaudio.paInt16,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=False,
+    frames_per_buffer=CHUNK)
 
-# unparsed data to see if mic works
-for i in range(20*44100/1024):  # Read multiple data
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    peak=np.average(np.abs(data))*2
-    bars="▬"*int(50*peak/2**16)
-    print("%04d %05d %s"%(i,peak,bars))
 
-# close the stream gracefully
-stream.stop_stream()
-stream.close()
-p.terminate()
+while True:
+    try:  # unparsed data to see if mic works
+        data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+        peak=np.average(np.abs(data))*2
+        bars="▬"*int(50*peak/2**16)
+        print("%04d %05d %s"%(peak,bars))   # prints audio level as bars
+
+    except KeyboardInterrupt:  # closes on keystroke detection
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
