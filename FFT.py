35f5cf0c5015419da3a174b91ebdc78879e31ab8
Hudki <42683289+Hudki@users.noreply.github.com>
2019-08-23 22:52:41 -0400

    Parsing correct information

--- FFT.py
+++ FFT.py
@@ -8,7 +8,7 @@ from tkinter import TclError
 
 fig, ax = plt.subplots(1, figsize=(15, 7))
 
-CHUNK = 1024*4
+CHUNK = 1028*4
 CHANNELS = 1
 FREQUENCY =  44100 # Hz
 
@@ -32,16 +32,12 @@ line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
 ax.set_title('AUDIO WAVEFORM')
 ax.set_xlabel('samples')
 ax.set_ylabel('volume')
-ax.set_ylim(0, 255)
+ax.set_ylim(-2**15,2**15)
 ax.set_xlim(0, 2 * CHUNK)
-plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 256, 255])
 
 # show the plot
 plt.show(block=False)
-
-# for measuring frame rate
-frame_count = 0
-start_time = time.time()
 
 while True:
 
@@ -52,21 +48,11 @@ while True:
     data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
 
     # create np array and offset by 128
-    data_np = np.array(data_int, dtype='b')[::2] + 128
+    data_np = np.frombuffer(data, dtype=np.int16)
 
     line.set_ydata(data_np)
 
     # update figure canvas
-    try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        frame_count += 1
 
-    except TclError:
-
-        # calculate average frame rate
-        frame_rate = frame_count / (time.time() - start_time)
-
-        print('stream stopped')
-        print('average frame rate = {:.0f} FPS'.format(frame_rate))
-        break
+    fig.canvas.draw()
+    fig.canvas.flush_events()
